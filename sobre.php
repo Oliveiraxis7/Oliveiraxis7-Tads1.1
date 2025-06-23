bdb101afb93f96aac8167bcf25f9df0527756e5b
Oliveira <gb1212go@gmail.com>
2025-06-22 22:02:42 -0300

    AAAAAAAAAAAAAAAAA

--- sobre.php
+++
@@ -1,6 +0,0 @@
-<?php include('header.php'); ?>
-
-<h2>Sobre a Fp Implementos</h2>
-<p>A Fp Implementos atua na venda de veículos pesados e leves, oferecendo produtos de qualidade, atendimento confiável e transparência em todas as negociações.</p>
-
-<?php include('footer.php'); ?>
